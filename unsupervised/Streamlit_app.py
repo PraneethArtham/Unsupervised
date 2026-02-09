72a3e7dc265306b787e92fa88d71f2094472b93e
PraneethArtham <arthampraneeth977@gmail.com>
2026-02-09 15:12:29 +0530

    Update Streamlit_app.py

--- unsupervised/Streamlit_app.py
+++ unsupervised/Streamlit_app.py
@@ -14,20 +14,20 @@ def load_model():
     if not os.path.exists("kmeans_model.pkl"):
         st.error("Model file missing. Upload kmeans_model.pkl to repo.")
         st.stop()
-    return joblib.load("kmeans_model.pkl")
+    return joblib.load("unsupervised/kmeans_model.pkl")
 
 
 @st.cache_resource
 def load_scaler():
-    return joblib.load("scaler.pkl")
+    return joblib.load("unsupervised/scaler.pkl")
 
 @st.cache_data
 def load_data():
-    return pd.read_csv("Mall_Customers.csv")
+    return pd.read_csv("unsupervised/Mall_Customers.csv")
 
 @st.cache_data
 def load_clustered():
-    return pd.read_csv("clustered_mall_customers.csv")
+    return pd.read_csv("unsupervised/clustered_mall_customers.csv")
 
 model = load_model()
 scaler = load_scaler()
@@ -85,4 +85,5 @@ if st.button("Predict Cluster"):
     )
 
     st.plotly_chart(fig, use_container_width=True)
+
 
